9bd3a3b1220eb8c8f5741983882b557433518e04
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-05-20 23:10:47 +0530

    acessing and assigning variable

--- block-BJaaaj/code/index.js
+++ block-BJaaaj/code/index.js
@@ -6,17 +6,28 @@ let lastName = "Stark";
 // Do the following:
 
 // Alert the value of the firstName.
+alert(firstName);
 // Alert the value of the lastName.
+alert(lastName);
 // Assign a new value of `"John"` to firstName.
+let Arya = "John";
 // Assign a new value of `"Snow"` to lastName.
+let Stark = "Snow";
 // Alert the new value of `firstName` and `lastName` by concatenating them
+alert(firstName + lastName);
+
 // Create a new varaible using `const` named `fullName` and assign the fullname using firstName and lastName seperated by space.
+const fullName = "Arya Stark";
 
 // Declare three variables `name,` `newUser` and `isAdmin`.
 // Assign your name as the value of `name`.
+let name = "Vikram";
 // Assign the value of `name` to `newUser`.
+let newUser = "Vikram";
 // Set the value of `isAdmin` to `true`.
+let isAdmin = true;
 // Alert the values of all the three variables.
+alert(newUser + isAdmin);
 
 // Assignment and re-assignment
 
@@ -25,9 +36,15 @@ var wiseMan = "Tyrion Lannister";
 // Follow instructions and do the following. In case of an error write the error in comment
 
 // Reassign the value of `wiseMan` to "Samwell Tarly"
+let Tyrion_Lannister = "Samwell Tarly";
+
 // Declare a variable `userName` with value "Lysa Arryn"
+let userName = "Lysa Arryn";
 // Declare a variable named `oddNumber` and assign a value `57`.
+let oddNumber = 57;
+
 // Reassign the value of `oddNumber` to 61
+
 
 // Mathematical Operation
 
@@ -36,6 +53,12 @@ let amount = 4280;
 // Using mathematical operations find the solutions. `(+, -, \*, / , etc.)`
 
 // Declare a new variable `reducedAmount`. In it store the value that is 24 less than the value of amount.
+let reducedAmount = 4280-24;
+
 // Declare another variable `addedAmount`. It's value should be 32 more than the value of amount.
+let addedAmount = 4280+32;
 // Declare a variable `multipleAmount`. It's value should be 7 times the value of amount.
+let multipleAmount = 4280*7;
 // Declare a variable `dividedAmount`. It should store the resultant of amount divided by 57.
+let dividedAmount = 4280/57;
+
