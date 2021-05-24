44ce7649608e395f3fd44fd81ad330ec343ace7d
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-05-24 19:12:41 +0530

    Bjaaaw

--- block-BJaaaw/code/index.js
+++ block-BJaaaw/code/index.js
@@ -7,9 +7,29 @@
   - `if` the number is odd print the message "number is odd"
 */
 
+let number = Number(prompt('enter the number?'));
+
+if (number % 2 === 0 ) { alert("number is even");}
+    else{ alert("number is odd");}
+
+
+
+
 // 2. Write a program to accept two numbers from user using`prompt` and alert the max value.
 
+let a = Number(prompt('enter first number?'));
+
+let b = Number(prompt('enter second number?'));
+
+if (a>b) { alert( `${a} is greater`);}
+   else { alert(`${b} is greater`);};
+   
+
+
 // 3. Convert the above code using`?` terniary operator
+
+
+(a>b) ? alert(a) : alert(b);
 
 /*
 4. Write a program that asks the user for the house name and check the following conditions:
@@ -19,11 +39,24 @@
   - `else` print the message " All men must die"
 */
 
+
+let housename = prompt("enter house name");
+
+if (housename === "stark") {alert("winter is coming");}
+  else if (housename === "lannister") {alert("A lannister always pays his debt");}
+  else{alert("All men must die");}
+
 // 5. Convert the above code using`?` terniary operator
+
+(housename ==="stark") ? alert("winter is coming") : alert("all men die") ;
+(housename === "lannister") ? alert(" lannister always pays his debt") : alert("all men die") ;
+
 
 // Switch
 
 // 6. Write a program that takes the number of the month(1 - 12) and alert number of days in the month.
+
+
 
 /* 7.
 - Write a program that take the salery of the user using prompt and alert the in -hand amount.You will find out the in -hand amount by deducting the tax amoun from salery.Conditions are given below.
